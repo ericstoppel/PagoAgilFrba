3d4b4d69d12d15764cd19304e77cd0c7ba484368
Tomas Chejanovich <tomycheja@gmail.com>
2017-11-28 19:58:36 -0300

    Script terminado

--- src/PagoAgilFrba/Rendicion/ListadoRendiciones.Designer.cs
+++ src/PagoAgilFrba/Rendicion/ListadoRendiciones.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.Table_Rendiciones = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Table_Rendiciones)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,33 +42,21 @@
             this.Table_Rendiciones.TabIndex = 0;
             this.Table_Rendiciones.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Table_Rendiciones_CellClick);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(213, 246);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Doble click para ver detalle de la rendicion";
-            // 
             // ListadoRendiciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(666, 271);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.Table_Rendiciones);
             this.Name = "ListadoRendiciones";
             this.Text = "ListadoRendiciones";
             ((System.ComponentModel.ISupportInitialize)(this.Table_Rendiciones)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView Table_Rendiciones;
-        private System.Windows.Forms.Label label1;
     }
 }
