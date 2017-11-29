3cd6cc8d76ebcdc1e47f9c670747b2d747af8c6d
Tomas Chejanovich <tomycheja@gmail.com>
2017-11-28 22:17:08 -0300

    Ultimos cambios

--- src/PagoAgilFrba/AbmSucursal/EditarSucursal.cs
+++ src/PagoAgilFrba/AbmSucursal/EditarSucursal.cs
@@ -61,36 +61,9 @@ namespace PagoAgilFrba.AbmSucursal
                 MessageBox.Show("El campo activo tiene que ser un 1 o un 0.");
                 return false;
             }
-            else if (!ValidarCodigoPostal(CodigoPostal))
-            {
-                MessageBox.Show("El codigo postal ingresado ya esta en uso por otra sucursal.");
-                return false;
-            }
             else
             {
                 return true;
-            }
-        }
-
-        private Boolean ValidarCodigoPostal(String CodigoPostal)
-        {
-            SqlServer sql = new SqlServer();
-            var listaParametros = new Dictionary<string, string>();
-            listaParametros.Add("codigo_postal", CodigoPostal);
-            DataTable tabla = sql.EjecutarSp("SP_Validar_Codigo_Postal_Sucursal", listaParametros);
-
-            if (tabla.Rows.Count > 0 && tabla.Rows[0].ItemArray[0].ToString() == "ERROR")
-            {
-                MessageBox.Show(tabla.Rows[0].ItemArray[1].ToString());
-                return false;
-            }
-            else if (tabla.Rows.Count == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
             }
         }
 
